f1372ef27b5849eeab2a6bdd164879f8df15b962
Xabi <xartolaza18dw@ikzubirimanteo.com>
2018-11-09 09:33:13 +0100

    He creado un archivo java de prueba

---
+++ src/Prueba2.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ubuntu
+ *
+ */
+public class Prueba2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Prueba");
+	}
+
+}
